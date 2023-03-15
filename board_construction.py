47f469ea659c750ada8d80631e46af1274aacac7
Paul Selling <48974493+pselling520@users.noreply.github.com>
2023-03-15 19:51:27 -0400

    Add files via upload

---
+++ board_construction.py
@@ -0,0 +1,25 @@
+class Board:
+    def __init__(self,
+                 tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9,
+                 til10, tile11, tile12, tile13, tile14, tile15, tile16, tile17,
+                 tile18, tile19):
+
+        self.tile1 = tile1
+        self.tile2 = tile2
+        self.tile3 = tile3
+        self.tile4 = tile4
+        self.tile5 = tile5
+        self.tile6 = tile6
+        self.tile7 = tile7
+        self.tile8 = tile8
+        self.tile9 = tile9
+        self.tile10 = tile10
+        self.tile11 = tile11
+        self.tile12 = tile12
+        self.tile13 = tile13
+        self.tile14 = tile14
+        self.tile15 = tile15
+        self.tile16 = tile16
+        self.tile17 = tile17
+        self.tile18 = tile18
+        self.tile19 = tile19
